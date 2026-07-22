46915ffa09f7f119ad03981f70445cc1d52d4571
FelipepPrado <felipepdelimaa@gmail.com>
2026-07-22 09:49:24 -0300

    Initial Commit

---
+++ WorkShop/WorkShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkShopApp.swift
+//  WorkShop
+//
+//  Created by Felipe Prado de Lima on 22/07/26.
+//
+
+import SwiftUI
+
+@main
+struct WorkShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
