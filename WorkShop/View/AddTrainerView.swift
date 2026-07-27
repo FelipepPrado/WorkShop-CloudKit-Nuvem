ca3471684a5fc4c017de23578ef7209cc1cea884
FelipepPrado <felipepdelimaa@gmail.com>
2026-07-27 09:26:32 -0300

    fix: sort in trainers

--- WorkShop/View/AddTrainerView.swift
+++ WorkShop/View/AddTrainerView.swift
@@ -46,6 +46,7 @@ struct AddTrainerView: View {
                 
                 //Ele só adicionar um elemento novo, se conseguir salvar o elemento no CloudKit
                 trainers.append(newTrainer.observable)
+                trainers =  trainers.sorted(by: { $0.name < $1.name })
             } catch{
                 print(error)
             }
